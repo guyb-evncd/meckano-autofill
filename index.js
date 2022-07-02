94b35cac407d7bb713de5e0659f3a56f08a643f9
Guy Brukhis <guy.brukhis@evinced.com>
2022-07-02 15:53:46 +0300

    prettier

--- index.js
+++ index.js
@@ -6,48 +6,49 @@
 */
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms)
-    })
-  }
-  
-  async function waitFor(selector, parent = document) {
-    let attempts = 10
-    while (attempts > 0) {
-      const element = parent.querySelector(selector)
-      if (element) {
-        return element
-      }
-      await sleep(50)
-      attempts--
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+async function waitFor(selector, parent = document) {
+  let attempts = 10
+  while (attempts > 0) {
+    const element = parent.querySelector(selector)
+    if (element) {
+      return element
     }
-    throw new Error(`Could not find ${selector}`)
+    await sleep(50)
+    attempts--
   }
-  
-  function getNonRestDays() {
-    return document.querySelectorAll('table.employee-report > tbody > tr[data-report_data_id]:not(.highlightingRestDays)')
+  throw new Error(`Could not find ${selector}`)
+}
+
+function getNonRestDays() {
+  return document.querySelectorAll(
+    'table.employee-report > tbody > tr[data-report_data_id]:not(.highlightingRestDays)'
+  )
+}
+
+function getMissingDays(nonRestDays) {
+  return Array.from(nonRestDays).filter(tr => tr.querySelector('.missing').innerText === '+')
+}
+
+async function submitHours(day) {
+  day.querySelector('a.insert-row').click()
+  const insertRow = await waitFor('tr.insert-row')
+  insertRow.querySelector('input.checkin-str').value = '09:00'
+  insertRow.querySelector('input.checkout-str').value = '18:00'
+  insertRow.querySelector('button.inline-confirm').click()
+  await sleep(1000)
+}
+
+async function fillMonth() {
+  let nonRestDays = getNonRestDays()
+  let missingDays = getMissingDays(nonRestDays)
+  while (missingDays.length > 0) {
+    await submitHours(missingDays[0])
+    nonRestDays = getNonRestDays()
+    missingDays = getMissingDays(nonRestDays)
   }
-  
-  function getMissingDays(nonRestDays) {
-    return Array.from(nonRestDays).filter(tr => tr.querySelector('.missing').innerText === '+')
-  }
-  
-  async function submitHours(day) {
-    day.querySelector('a.insert-row').click()
-    const insertRow = await waitFor('tr.insert-row')
-    insertRow.querySelector('input.checkin-str').value = '09:00'
-    insertRow.querySelector('input.checkout-str').value = '18:00'
-    insertRow.querySelector('button.inline-confirm').click()
-    await sleep(1000)
-  }
-  
-  async function fillMonth() {
-    let nonRestDays = getNonRestDays()
-    let missingDays = getMissingDays(nonRestDays)
-    while (missingDays.length > 0) {
-      await submitHours(missingDays[0])
-      nonRestDays = getNonRestDays()
-      missingDays = getMissingDays(nonRestDays)
-    }
-  }
-  
+}
